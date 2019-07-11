61ca941b759430cf40fe1650b794e0aa676538d1
believelody <believelody@gmail.com>
2019-07-11 10:35:24 +0200

    SidenoteBtn
    Enable transition move when btn goes back

--- src/components/sidenotes/SidenoteBtn.jsx
+++ src/components/sidenotes/SidenoteBtn.jsx
@@ -1,123 +1,134 @@
-import React, { useRef, useEffect, useState } from 'react'
-import styled, { keyframes } from 'styled-components'
-import { arrowToRight } from '../../utils/arrowKeyframes'
-import { usePanelValues } from '../../context/panelContext';
-import { EXPAND, SET_COMPONENT } from '../../reducer/panelReducer';
-
-const slideBtn = keyframes`
-    to { right: 110vw; }
-`
+import React, { useRef, useEffect, useState } from "react";
+import styled from "styled-components";
+import { arrowToRight } from "../../utils/arrowKeyframes";
+import { usePanelValues } from "../../context/panelContext";
+import { SET_COMPONENT } from "../../reducer/panelReducer";
 
 const BtnStyle = styled.div`
-    position: fixed;
-    margin: 0;
-    padding: 0;
-    bottom: 10px;
-    right: ${props => props.x}px;
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    background-color: red;
-    display: flex;
-    justify-content: center;
-    align-items center;
-    color: white;
-    overflow: hidden;
-    transform:  scale(${props => props.isHold ? 1.3 : 1});
-    transition: transform ${props => props.duration}ms ease, right ${props => props.x < 150 ? 0 : props.duration * 3}ms ease;
-    z-index: 100;
+  position: fixed;
+  margin: 0;
+  padding: 0;
+  bottom: 10px;
+  right: ${props => props.x}px;
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  background-color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  overflow: hidden;
+  transform: scale(${props => (props.isHold ? 1.3 : 1)});
+  transition: transform ${props => props.duration}ms ease,
+    right
+      ${props =>
+        props.x < props.threshold && props.isHold ? 0 : props.duration * 3}ms
+      ease;
+  z-index: 100;
 
-    & > section {
-        position: absolute;
-        display: grid;
-        width: 100px;
-        grid-template-columns: repeat(2, 50%);
-        transform: translateX(${props => props.isHold ? -25 : 25}%);
-        transition: transform ${props => props.duration}ms ease;
+  & > section {
+    position: absolute;
+    display: grid;
+    width: 100px;
+    grid-template-columns: repeat(2, 50%);
+    transform: translateX(${props => (props.isHold ? -25 : 25)}%);
+    transition: transform ${props => props.duration}ms ease;
 
-        & > span { 
-            width: 100%; 
-            text-align: center;
-        }
-
-        & .second {
-            font-size: 12px;
-        }
-
-        & .second > i:nth-child(1) {
-            animation: ${arrowToRight} 1000ms ${props => props.duration + 100}ms ease-in infinite;
-        }
-
-        & .second > i:nth-child(2) {
-            animation: ${arrowToRight} 1000ms ${props => props.duration + 200}ms ease-in infinite;
-        }
-
-        & .second > i:nth-child(3) {
-            animation: ${arrowToRight} 1000ms ${props => props.duration + 300}ms ease-in infinite;
-        }
+    & > span {
+      width: 100%;
+      text-align: center;
     }
-`
+
+    & .second {
+      font-size: 12px;
+    }
+
+    & .second > i:nth-child(1) {
+      animation: ${arrowToRight} 1000ms ${props => props.duration + 100}ms
+        ease-in infinite;
+    }
+
+    & .second > i:nth-child(2) {
+      animation: ${arrowToRight} 1000ms ${props => props.duration + 200}ms
+        ease-in infinite;
+    }
+
+    & .second > i:nth-child(3) {
+      animation: ${arrowToRight} 1000ms ${props => props.duration + 300}ms
+        ease-in infinite;
+    }
+  }
+`;
 
 const SidenoteBtn = () => {
-    const X_POS = 10
-    const duration = 400
+  const X_POS = 10;
+  const DURATION = 400;
+  const THRESHOLD = 250;
 
-    const btnRef = useRef()
+  const btnRef = useRef();
 
-    const [isHold, setHold] = useState(false)
-    const [btnX, setBtnX] = useState(X_POS)
-    
-    const [{expand}, dispatch] = usePanelValues()
+  const [isHold, setHold] = useState(false);
+  const [btnX, setBtnX] = useState(X_POS);
 
-    const moveBtn = e => {
-        let screenWidth = window.screen.width
-        let x = screenWidth - e.nativeEvent.clientX - btnRef.current.getBoundingClientRect().width - X_POS
-        if (btnX < 150 && isHold) {
-            setBtnX(x)
-        }
-        else if (isHold) {
-            setBtnX(screenWidth + 20)
-            dispatch({ type: SET_COMPONENT, payload: 'sidenote' })
-        }
+  const [{ expand }, dispatch] = usePanelValues();
+
+  const moveBtn = e => {
+    let screenWidth = window.screen.width;
+    let x =
+      screenWidth -
+      e.pageX -
+      btnRef.current.getBoundingClientRect().width -
+      X_POS;
+    if (btnX < THRESHOLD && isHold) {
+      setBtnX(x);
+    } else if (isHold) {
+      alert(btnX);
+      setBtnX(screenWidth + 20);
+      dispatch({ type: SET_COMPONENT, payload: "sidenote" });
     }
+  };
 
-    const resetMoveBtn = () => setBtnX(X_POS)
+  const resetMoveBtn = () => setBtnX(X_POS);
 
-    const handleMouseClick = (isHold) => {
-        if (btnX < 150) setHold(isHold)
-        if (!isHold) resetMoveBtn()
+  const handleMouseClick = isHold => {
+    if (btnX < THRESHOLD) setHold(isHold);
+    if (!isHold) resetMoveBtn();
+  };
+
+  useEffect(() => {
+    if (!expand) {
+      setHold(false);
+      resetMoveBtn();
     }
+  }, [expand]);
 
-    useEffect(() => {
-        if (!expand) {
-            setHold(false)
-            resetMoveBtn()
-        }
-    }, [expand])
+  return (
+    <BtnStyle
+      ref={btnRef}
+      onMouseDown={() => handleMouseClick(true)}
+      onMouseUp={() => handleMouseClick(false)}
+      onMouseMove={moveBtn}
+      onTouchStart={() => handleMouseClick(true)}
+      onTouchEnd={() => handleMouseClick(false)}
+      onTouchMove={moveBtn}
+      isHold={isHold}
+      x={btnX}
+      duration={DURATION}
+      threshold={THRESHOLD}
+    >
+      <section>
+        <span className="first">
+          <i className="fas fa-comment-dots" />
+        </span>
+        <span className="second">
+          <i className="fas fa-chevron-left" />
+          <i className="fas fa-chevron-left" />
+          <i className="fas fa-chevron-left" />
+        </span>
+      </section>
+    </BtnStyle>
+  );
+};
 
-    return (
-        <BtnStyle 
-            ref={btnRef}
-            onMouseDown={() => handleMouseClick(true)} 
-            onMouseUp={() => handleMouseClick(false)}
-            onMouseMove={moveBtn}
-            onTouchStart={() => handleMouseClick(true)} 
-            onTouchEnd={() => handleMouseClick(false)}
-            onTouchMove={moveBtn}
-            isHold={isHold}
-            x={btnX}
-            duration={duration}
-        >
-            <section>
-                <span className='first'><i className='fas fa-comment-dots'></i></span>
-                <span className='second'>
-                    <i className='fas fa-chevron-left'></i>
-                    <i className='fas fa-chevron-left'></i>
-                    <i className='fas fa-chevron-left'></i>
-                </span>
-            </section>
-        </BtnStyle>
-    )
-}
-
-export default SidenoteBtn
+export default SidenoteBtn;
--- src/components/tooltip/Tooltip.jsx
+++ src/components/tooltip/Tooltip.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
-import devices from '../../utils/devices';
+import React from "react";
+import styled from "styled-components";
+import devices from "../../utils/devices";
 
 const TooltipStyle = styled.span`
-    position: absolute;
-    top: -8%;
-    left: 50%;
-    transform: translateX(-50%);
-    border-radius: 5px;
-    color: white;
-    border: 2px solid darkgreen;
+  position: absolute;
+  top: -8%;
+  left: 50%;
+  transform: translateX(-50%);
+  border-radius: 5px;
+  color: white;
+  border: 2px solid darkgreen;
+  background-color: white;
+  color: black;
 
-    @media ${devices.mobileL} {
-        top: -1%;
-        font-size: .6em;
+  @media ${devices.mobileL} {
+    top: -1%;
+    font-size: 0.6em;
 
-        @media (orientation: landscape) {visibility: hidden;}
+    @media (orientation: landscape) {
+      visibility: hidden;
     }
-`
+  }
+`;
 
 const Tooltip = ({ children }) => {
-    return (
-        <TooltipStyle>
-            {children}
-        </TooltipStyle>
-    )
-}
+  return <TooltipStyle>{children}</TooltipStyle>;
+};
 
-export default Tooltip
+export default Tooltip;
